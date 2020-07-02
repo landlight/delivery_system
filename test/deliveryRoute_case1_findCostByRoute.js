e38cf21e2fb455ae9fd69dbf1c61b8aac3dd10de
landlight <ck.light@gmail.com>
2020-07-02 17:31:49 +0700

    :white_check_mark: create test cases for deliveryCost Case 1

--- test/deliveryRoute_case1_findCostByRoute.js
+++ test/deliveryRoute_case1_findCostByRoute.js
@@ -4,6 +4,7 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+let db = require('../database');
 
 chai.use(chaiHttp);
 
@@ -93,3 +94,100 @@ describe('GET /api/deliveryRoute/findCostByRoute', () => {
         });
     });
 });
+
+// Message No Path Route and Correct Test
+describe('3. No Path Route and Correct Test', () => {
+    before((done) => { //Before each test we empty the database
+        setTimeout(function() {
+            db.get().dropDatabase(() => {
+                done();
+            })
+        }, 200); 
+    });
+    describe('Create Data for testing  /api/deliveryRoute/findCostByRoute', () => {
+        it('3.1. Create Data Paths for testing Case 1', (done) => {
+            chai.request(server)
+                .post('/api/deliveryRoute')
+                .send({deliveryRoute: "AB3"})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                done();
+            });
+        });
+        it('3.2. Create Data Paths for testing Case 1', (done) => {
+            chai.request(server)
+                .post('/api/deliveryRoute')
+                .send({deliveryRoute: "BC4"})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                done();
+            });
+        });
+        it('3.3. Create Data Paths for testing Case 1', (done) => {
+            chai.request(server)
+                .post('/api/deliveryRoute')
+                .send({deliveryRoute: "CE10"})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                done();
+            });
+        });
+        it('3.4. Create Data Paths for testing Case 1', (done) => {
+            chai.request(server)
+                .post('/api/deliveryRoute')
+                .send({deliveryRoute: "EF2"})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                done();
+            });
+        });
+        it('3.5. No Path Route', (done) => {
+            chai.request(server)
+                .get('/api/deliveryRoute/findCostByRoute')
+                .query({deliveryPath: 'A-B-D-E'})
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property('message');
+                    res.body.message.should.be.eql("No Such Route");
+                done();
+            });
+        });
+        it('3.6. No Path Route', (done) => {
+            chai.request(server)
+                .get('/api/deliveryRoute/findCostByRoute')
+                .query({deliveryPath: 'A-B-E-F'})
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property('message');
+                    res.body.message.should.be.eql("No Such Route");
+            done();
+            });
+        });
+        it('3.7. Correct Path => expect 7', (done) => {
+            chai.request(server)
+                .get('/api/deliveryRoute/findCostByRoute')
+                .query({deliveryPath: 'A-B-C'})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('deliveryCost');
+                    res.body.deliveryCost.should.be.eql(7); // 3 + 4
+            done();
+            });
+        });
+        it('3.8. Correct Path => expect 16', (done) => {
+            chai.request(server)
+                .get('/api/deliveryRoute/findCostByRoute')
+                .query({deliveryPath: 'B-C-E-F'})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('deliveryCost');
+                    res.body.deliveryCost.should.be.eql(16); // 4 + 10 + 2
+            done();
+            });
+        });
+    });
+});  
+
+
+
+    
